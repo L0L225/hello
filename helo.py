7306462a2c9e14f7066781bfa79cef49fe6ae6e4
L0L225 <33068919+L0L225@users.noreply.github.com>
2020-05-10 13:39:51 +0300

    Add files via upload

--- helo.py
+++ helo.py
@@ -1,6 +1,4 @@
-
-print('ver 0.1')
-print('\n 1. hello\n 2. world\n 3. 1+2\n 4. up\n')
+import os, time
 
 a1 = 'hello '
 a2 = 'world'
@@ -9,17 +7,21 @@ a3 = a1 + a2
 def up():
 	exec('git pull https://github.com/L0L225/hello.git')
 
-
-
-a = int(input('> '))
-if a == 1:
-	print(a1)
-elif a == 2:
-	print(a2)
-elif a == 3:
-	print(a3)
-elif a == 4:
-	up()
-else:
-	print('err')
-
+def main():
+	os.system('clear')
+	print('ver 0.2')
+	print('\n 1. hello\n 2. world\n 3. 1+2\n 4. up\n')
+	a = int(input('> '))
+	if a == 1:
+		print(a1)
+	elif a == 2:
+		print(a2)
+	elif a == 3:
+		print(a3)
+	elif a == 4:
+		up()
+	else:
+		print('err')
+	time.sleep(3)
+while True:
+	main()
