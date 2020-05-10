ccda2a50f88336e7ff0280c9de3020ba2c70fe03
L0L225 <33068919+L0L225@users.noreply.github.com>
2020-05-10 11:23:12 +0300

    Add files via upload

--- helo.py
+++ helo.py
@@ -1,1 +1,26 @@
-print('hello world')
+
+
+print('\n 1. hello\n 2. world\n 3. 1+2\n 4. up\n')
+
+a1 = 'hello '
+a2 = 'world'
+a3 = a1 + a2
+
+def up():
+	exec(cd -)
+	exec(git clone https://github.com/L0L225/hello.git)
+
+
+
+a = int(input('> '))
+if a == 1:
+	print(a1)
+elif a == 2:
+	print(a2)
+elif a == 3:
+	print(a3)
+elif a == 4:
+	up()
+else:
+	print('err')
+
